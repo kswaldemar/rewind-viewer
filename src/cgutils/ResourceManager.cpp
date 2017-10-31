03ac15b4d31d1388b8a2ebd070cf263b2bd0168c
Vladimir Kiselev <ski3.mdn@yandex.ru>
2017-10-31 23:03:54 +0300

    Support textures loading in ResourceManager

--- src/cgutils/ResourceManager.cpp
+++ src/cgutils/ResourceManager.cpp
@@ -1,19 +1,19 @@
-//
-// Created by valdemar on 22.10.17.
-//
-
 #include "ResourceManager.h"
 
 #include <common/logger.h>
-#include <cstring>
 
-namespace {
+#include <stb_image.h>
 
-} // anonymous namespace
+ResourceManager::ResourceManager(const std::string &path_to_res_folder)
+    : res_folder_(path_to_res_folder)
+{
+    stbi_set_flip_vertically_on_load(true);
+}
 
 ResourceManager::~ResourceManager() {
     glDeleteBuffers(static_cast<GLsizei>(buffers_.size()), buffers_.data());
     glDeleteVertexArrays(static_cast<GLsizei>(vaos_.size()), vaos_.data());
+    glDeleteTextures(static_cast<GLsizei>(textures_.size()), textures_.data());
 }
 
 GLuint ResourceManager::gen_vertex_array() {
@@ -28,4 +28,47 @@ GLuint ResourceManager::gen_buffer() {
     glGenBuffers(1, &buf);
     buffers_.push_back(buf);
     return buf;
+}
+
+GLuint ResourceManager::load_texture(const std::string &path_to_texture, bool gen_mipmap,
+                                     GLint wrap_s, GLint wrap_t, GLint flt_min, GLint flt_mag) {
+    int width;
+    int height;
+    int nr_channels;
+    unsigned char *data = stbi_load(path_to_texture.c_str(), &width, &height, &nr_channels, 0);
+    if (!data) {
+        LOG_WARN("Cannot load texture %s", path_to_texture.c_str());
+        return 0;
+    }
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    textures_.push_back(tex);
+
+    GLenum format;
+    if (nr_channels == 3) {
+        format = GL_RGB;
+    } else if (nr_channels == 4) {
+        format = GL_RGBA;
+    } else {
+        format = GL_RED;
+    }
+
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+
+    if (gen_mipmap) {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, flt_min);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, flt_mag);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_s);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_t);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    stbi_image_free(data);
+
+    return tex;
 }
--- src/cgutils/ResourceManager.h
+++ src/cgutils/ResourceManager.h
@@ -15,14 +15,24 @@
  */
 class ResourceManager {
 public:
+    ResourceManager(const std::string &path_to_res_folder = "");
     ~ResourceManager();
 
     GLuint gen_vertex_array();
     GLuint gen_buffer();
+    GLuint load_texture(const std::string &path_to_texture,
+                        bool gen_mipmap = true,
+                        GLint wrap_s = GL_CLAMP_TO_EDGE,
+                        GLint wrap_t = GL_CLAMP_TO_EDGE,
+                        GLint flt_min = GL_LINEAR,
+                        GLint flt_mag = GL_LINEAR);
 
 private:
     std::vector<GLuint> vaos_;
     std::vector<GLuint> buffers_;
+    std::vector<GLuint> textures_;
+
+    std::string res_folder_;
 };
 
 
