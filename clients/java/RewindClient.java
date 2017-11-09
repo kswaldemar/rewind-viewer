a3071def2d760e94325178b6d9edf6976f87d3d4
dmitry <my@my.ru>
2017-11-10 01:01:04 +0300

    - update java client to 1.2 api; - added main method for testing

--- clients/java/RewindClient.java
+++ clients/java/RewindClient.java
@@ -7,25 +7,7 @@ import java.net.Socket;
 /**
  * Java client for Rewind viewer. Put this file to the same default package where Strategy/MyStrategy/Runner and other
  * files are extracted.
- * <p>
- * Sample usage:
- * <pre>
- * {@code
- *
- *  private final RewindClient rewindClient = new RewindClient();
- *
- *  @Override
- *  public void move(Wizard self, World world, Game game, Move move) {
- *      initializeTick(self, world, game, move);
- *      for (Wizard w : world.getWizards()) {
- *          RewindClient.Side side = w.getFaction() == self.getFaction() ? RewindClient.Side.OUR : RewindClient.Side.ENEMY;
- *          rewindClient.livingUnit(w.getId(), w.getX(), w.getY(), w.getRadius(), w.getLife(), w.getMaxLife(), side);
- *      }
- *      ...
- *      rewindClient.endFrame();
- *  }
- * }
- * </pre>
+ * Sample usage: look at main method
  */
 public class RewindClient {
 
@@ -40,6 +22,19 @@ public class RewindClient {
 
         Side(int side) {
             this.side = side;
+        }
+    }
+
+    public enum AreaType {
+        UNKNOWN(0),
+        FOREST(1),
+        SWAMP(2),
+        RAIN(3),
+        CLOUD(4),;
+        final int areaType;
+
+        AreaType(int areaType) {
+            this.areaType = areaType;
         }
     }
 
@@ -64,7 +59,11 @@ public class RewindClient {
 
     void livingUnit(double x, double y, double r, int hp, int maxHp,
                     Side side) {
-        livingUnit(x, y, r, hp, maxHp, side, 0, 0);
+        livingUnit(x, y, r, hp, maxHp, side, 0, 0, 0, 0, false);
+    }
+
+    void areaDescription(int cellX, int cellY, AreaType areaType) {
+        send(String.format("{\"type\": \"area\", \"x\": %d, \"y\": %d, \"area_type\": %d}", cellX, cellY, areaType.areaType));
     }
 
     void close() {
@@ -83,26 +82,29 @@ public class RewindClient {
      * @param msg .
      */
     public void message(String msg) {
-        String s = "{\"type\": \"message\", \"message\" : \"" + msg + " \")";
+        String s = "{\"type\": \"message\", \"message\" : \"" + msg + " \"}";
         send(s);
     }
 
     /**
-     * @param x        - x pos of the unit
-     * @param y        - y pos of the unit
-     * @param r        - radius of the unit
-     * @param hp       - current health
-     * @param maxHp    - max possible health
-     * @param side     - owner of the unit
-     * @param course   rotation of the unit - angle in radians [0, 2 * pi) counter clockwise
-     * @param unitType - id of unit type (see UnitType enum: https://github.com/kswaldemar/rewind-viewer/blob/master/src/viewer/Frame.h)
-     *                 to set texture
+     * @param x           - x pos of the unit
+     * @param y           - y pos of the unit
+     * @param r           - radius of the unit
+     * @param hp          - current health
+     * @param maxHp       - max possible health
+     * @param side        - owner of the unit
+     * @param course      - rotation of the unit - angle in radians [0, 2 * pi) counter clockwise
+     * @param unitType    - id of unit type (see UnitType enum: https://github.com/kswaldemar/rewind-viewer/blob/master/src/viewer/Frame.h)
+     * @param remCooldown -
+     * @param maxCooldown -
      */
     void livingUnit(double x, double y, double r, int hp, int maxHp,
-                    Side side, double course, int unitType) {
+                    Side side, double course, int unitType,
+                    int remCooldown, int maxCooldown, boolean selected) {
         send(String.format(
-                "{\"type\": \"unit\", \"x\": %f, \"y\": %f, \"r\": %f, \"hp\": %d, \"max_hp\": %d, \"enemy\": %d, \"unit_type\":%d, \"course\": %.3f}",
-                x, y, r, hp, maxHp, side.side, unitType, course));
+                "{\"type\": \"unit\", \"x\": %f, \"y\": %f, \"r\": %f, \"hp\": %d, \"max_hp\": %d, \"enemy\": %d, \"unit_type\":%d, \"course\": %.3f," +
+                        "\"rem_cooldown\":%d, \"cooldown\":%d, \"selected\":%d }",
+                x, y, r, hp, maxHp, side.side, unitType, course, remCooldown, maxCooldown, selected ? 1 : 0));
     }
 
     public RewindClient(String host, int port) {
@@ -126,5 +128,64 @@ public class RewindClient {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Example of use
+     *
+     * @param args .
+     */
+    public static void main(String[] args) {
+        RewindClient rc = new RewindClient();
+        int worldSize = 1024;
+        for (int x = 0; x < worldSize / 32; x++) {
+            for (int y = 0; y < worldSize / 32; y++) {
+                rc.areaDescription(x, y, AreaType.values()[(int) (Math.random() * AreaType.values().length)]);
+            }
+        }
+        rc.message("Hello World");
+        rc.endFrame();
+
+
+        for (int i = 0; i < 2000; i++) {
+            rc.message("Step " + i);
+            for (int j = 0; j < 100; j++) {
+                Color rndColor = new Color((int) (255 * 255 * 255 * Math.random()));
+
+
+                double unixX = Math.random() * worldSize;
+                double unitY = Math.random() * 32 + i;
+
+                rc.livingUnit(
+                        unixX,
+                        unitY,
+                        2 + Math.random() * 10,
+                        10,
+                        100,
+                        Side.values()[(int) (Side.values().length * Math.random())],
+                        Math.PI * Math.random(),
+                        1, //TODO enum
+                        (int) (60 * Math.random()),
+                        60,
+                        Math.random() > 0.8);
+
+                rc.line(unixX, unitY, Math.random() * worldSize, Math.random() * worldSize, rndColor);
+
+                double circleX = unixX + Math.random() * 40 - 20;
+                double circleY = unitY + Math.random() * 40 - 20;
+
+                rc.circle(circleX, circleY, 10 + Math.random() * 10, rndColor);
+
+                double rectX1 = circleX + Math.random() * 100;
+                double rectY1 = circleY + Math.random() * 100;
+                rc.rect(rectX1, rectY1, rectX1 + Math.random() * 40, rectY1 + Math.random() * 40, rndColor);
+            }
+            rc.endFrame();
+        }
+
+        rc.message("Bye!");
+        rc.endFrame();
+
+        rc.close();
     }
 }
